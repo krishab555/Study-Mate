a4c42b0295117a3e501e5ef457985248daf69ed7
krishab555 <kreeshabhandari111@gmail.com>
2025-09-02 19:32:01 +0545

    routes

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,12 +1,10 @@
-
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import ProtectedRoutes from "./ProctectedRoutes";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import DisscussionForum from "../pages/DisscussionForum";
 import Dashboard from "../pages/Dashboard";
-
 
 import StudentLayout from "./StudentLayout";
 import Profile from "../pages/student/Profile";
@@ -22,56 +20,61 @@ import Layout from "./Layout";
 const PageRoutes = () => {
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      {/* <Route path="/" element={<Layout />}>
-        {/* ✅ Default page when visiting "/" */}
-        {/* <Route index element={<Dashboard />} /> */} 
-      <Route path="/dashboard" element={<Layout><Dashboard />
-    </Layout>} />
 
+      {/* Default "/" goes to dashboard */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
 
-      <Route element={<ProtectedRoutes allowedRole={["Student"]}/>}>
+      {/* Or if you want redirect instead of showing Dashboard directly:
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      */}
+
+      <Route
+        path="/dashboard"
+        element={
+          <Layout>
+            <Dashboard />
+          </Layout>
+        }
+      />
+
+      {/* Student Routes */}
+      <Route element={<ProtectedRoutes allowedRole={["Student"]} />}>
         <Route path="/student" element={<StudentLayout />}>
-        <Route index element={<Home />} /> 
+          <Route index element={<Home />} />
           <Route path="home" element={<Home />} />
-          <Route path="courses" element={<Courses/>} />
+          <Route path="courses" element={<Courses />} />
           <Route path="profile" element={<Profile />} />
         </Route>
       </Route>
 
-
-
-        <Route element={<ProtectedRoutes allowedRole={["Instructor"]} />
-       }>
+      {/* Instructor Routes */}
+      <Route element={<ProtectedRoutes allowedRole={["Instructor"]} />}>
         <Route path="/instructor" element={<InstructorLayout />}>
-        <Route index element={<InstructorHome />} />
-
-        <Route path="home" element={<InstructorHome/>} />
-       </Route>
-          
-      </Route>
-
-
- <Route path="/discussionForum" element={<DisscussionForum/>} />
-      
-
-  
-      
-      
-      <Route element={<ProtectedRoutes allowedRole={["Admin"]} />
-       }>
-        <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<AdminHome />} />
-        <Route path="home" element={<AdminHome/>} />
+          <Route index element={<InstructorHome />} />
+          <Route path="home" element={<InstructorHome />} />
         </Route>
-          
       </Route>
 
+      {/* Discussion Forum */}
+      <Route path="/discussionForum" element={<DisscussionForum />} />
 
-        
+      {/* Admin Routes */}
+      <Route element={<ProtectedRoutes allowedRole={["Admin"]} />}>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHome />} />
+          <Route path="home" element={<AdminHome />} />
+        </Route>
+      </Route>
+
+      {/* Fallback route for unknown URLs */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
 
-
+export default PageRoutes;
